02629b590e037e8b5353ec9709ab8d0ecf84efa7
Rachael Beaton <rachael@Andromeda.(none)>
2015-02-26 17:05:53 -0500

    example optical photometry files

---
+++ example_splash_files/post_allframe/dbf.cmd
@@ -0,0 +1,16 @@
+##
+## 
+ID       real   %6.3f      # ID # first number indicates MiniMo Frame No.
+RA      real   %9.3f      # RA via WCS header position
+DEC     real   %9.3f      # DEC via WCS header position
+ID       real   %6.3f      # ID # first number indicates MiniMo Frame No.
+X        real   %9.3f      # X via chip no.
+Y        real   %9.3f      # Y via chip no. 
+D        real   %9.3f      # I magnitude
+D_ERR    real   %9.3f      # I magnitude error
+M        real   %9.3f      # I magnitude
+M_ERR    real   %9.3f      # I magnitude error
+T        real   %9.3f      # T magnitude
+T_ERR   real   %9.3f      # T magnitude error
+chi      real   %9.3f      # image quality statistic
+sharp    real   %9.3f      # image quality statistic
