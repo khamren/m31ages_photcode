30ac64121043e2c340cd94eb9392f4516ba9ad30
Rachael L. Beaton <rachael.l.beaton@gmail.com>
2015-02-27 12:01:05 -0500

    Update goodpsf.comments.f

--- daophot/goodpsf.comments.f
+++ daophot/goodpsf.comments.f
@@ -6,8 +6,15 @@ C This program generates a file with only good PSF candidates.
 C
 C=====================================================================
 C
+C -- This is declaring a bunch of variables
+C
       INTEGER nmax
       REAL minchi
+C
+C -- This is the internal daophot CHI value that is measuring "fluffiness"
+C -- A perfect star would have CHI=1
+C -- (CHI - 1.0) < 0.5 is a good set of cuts.
+C
       PARAMETER(nmax=1000,minchi=0.5)
 C
 C 'minchi' is the minimum CHI value allowed for a PSF star.
@@ -29,14 +36,23 @@ C
 C
 C=====================================================================
 C Count total number of PSF candidates.
+C -- READING IN THE FILE
 C=====================================================================
 C
       OPEN(11,FILE=lstfile)
+C
+C -- Reading the header of the lst file that contains the daophot gobbly gook
+C
       READ(11,'(A69)') line1
       READ(11,'(A69)') line2
       READ(11,*)
       nstar = 0
-      DO i = 1, nmax
+C
+C -- reading in each line of the file putting the parameters into 
+C --  arrays taht were initialized in the previous block
+C -- The columns are ID#, X pos, Y pos, Mag, Mag_err, Sky Value
+C 
+     DO i = 1, nmax
          READ(11,*,END=9) id(i),x(i),y(i),mag(i),err(i),sky(i)
          nstar = nstar + 1
       ENDDO
@@ -47,6 +63,18 @@ C Read in chi values and flags from .chi file.
 C Stole this part from Stetson's code (~line 580 of psf.f).
 C=====================================================================
 C
+C
+C -- this should be reading the section of the log file that contains
+C --  the chi-squred fits to the PSF
+C -- An example of this input is given in 
+C --                    /example_splash_files/allframe/obj175_1.psf.log
+C --  in lines 53-69.
+C -- its a stupid output: starid chi^2 optional flag
+C -- if a star is saturated it will say saturated instead of chi^2 
+C --  (this might be where the code breaks)
+C -- another option is chi^2 = defective, I don't know 100% what that means, but 
+C --  assumed it was really bad and I remove those stars.
+C 
       k = (nstar-1)/5 + 1
       OPEN(12,FILE=chifile)
       DO i = 1, k
@@ -57,18 +85,27 @@ C
          ENDDO
       ENDDO
       CLOSE(12)
+C -- format of the file
 69    FORMAT(I7, F7.3, 1X, A1, 1X)
 C           
 C=====================================================================
 C  Process PSF candidates flags and prune out.
 C=====================================================================
 C
+C -- number of stars from the readin in the previous line
       npsf = nstar
+C -- stupid complicated reading of the file
       OPEN(21,FILE=outfile)
       WRITE(21,'(A69)') line1
       WRITE(21,'(A69)') line2
       WRITE(21,*)
       DO i = 1, nstar
+C
+C -- check to see what the flags are, if its ? or *, then reject the star
+C --  practically, this is 2x and 3x sigma of the chi^2 values for the set of stars
+C -- Then its testing the values to fit in some parameter range
+C --
+C 
          IF (flag(i).NE.'?' .AND. flag(i).NE.'*' 
      .                      .AND. chi(i).LT.minchi) THEN
             DO j = 1, nstar
@@ -82,7 +119,11 @@ C
          ENDIF
 1     ENDDO
       CLOSE(21)
-70    FORMAT(I7,5F9.3)
+70    FORMAT(I7,5F9.3):
+C
+C -- Writing some results to the screen here based on the count of stars
+C --  where npsf is decremented for each star removed because there is
+C --  no way in fortran to say n_elements(array)
 C
       WRITE(6,71) npsf,nstar
 71    FORMAT(/'You have ',I3,' (out of ',I3,') PSF candidates left.'/)
